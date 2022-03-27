11c9f9b9b6add1e2221e001d11240d67231e2de5
Corenlix <corenlix@gmail.com>
2022-03-27 20:05:22 +0300

    Add health and pixel font

---
+++ Assets/Scripts/HealthView.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthView : MonoBehaviour
+{
+    [SerializeField] private Health _health;
+    [SerializeField] private Image _healthStripe;
+    [SerializeField] private TextMeshProUGUI _healthText;
+    
+    private void OnEnable()
+    {
+        _health.HealthChanged += UpdateHealthBar;
+    }
+
+    private void OnDisable()
+    {
+        _health.HealthChanged -= UpdateHealthBar;
+    }
+
+    private void UpdateHealthBar()
+    {
+        _healthStripe.fillAmount = (float)_health.CurrentHealth / _health.MaxHealth;
+        _healthText.text = $"{_health.CurrentHealth}/{_health.MaxHealth}";
+    }
+}
